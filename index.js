d57733c9216b05869f89e4bb19b862e875b51d4b
Reagin Turner <rturnerafteragnes@gmail.com>
2021-11-02 13:10:35 -0400

    Added index.js questions

--- index.js
+++ index.js
@@ -7,24 +7,40 @@ const markdown = require('./generatemarkdown')
 const start = [
     {
         type: 'input',
-        name: '',
-        message: ''
+        name: 'managerName',
+        message: 'Hello Team Manager, what is your name?'
     },
     {
-        type: '',
-        name: '',
-        message: 
+        type: 'input',
+        name: 'managerID',
+        message: 'What is your employee ID?'
     },
     {
-        type: '',
-        name: '',
-        message: ''
+        type: 'input',
+        name: 'managerEmail',
+        message: `Great! 
+         What is your email?`
     },
     {
-        type: '',
-        name: '',
-        message: ''
+        type: 'input',
+        name: 'managerOffNum',
+        message: 'Please enter your office numer.'
     },
+    {
+        type: 'list',
+        name: 'newMemb',
+        message: 'Would you like to add a new member or finish up?',
+        choices: ['Add new member', 'Finished']
+    },
+]
+
+const newMember = [
+    {
+        type: 'list',
+        name: 'membList',
+        message: 'Who would you like to add next?',
+        choices: ['Engineer', 'Intern']
+    }
 ]
 
 // create a function for the start of the code

0bee84d7c67b18bfd6c4d3f36c56921bd9c28aaf
Reagin Turner <rturnerafteragnes@gmail.com>
2021-11-02 13:29:59 -0400

    Added even more Q's; starting to add more functions

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ const start = [
         name: 'managerOffNum',
         message: 'Please enter your office numer.'
     },
+]
+const menu = [
     {
         type: 'list',
         name: 'newMemb',
@@ -33,7 +35,6 @@ const start = [
         choices: ['Add new member', 'Finished']
     },
 ]
-
 const newMember = [
     {
         type: 'list',
@@ -42,5 +43,80 @@ const newMember = [
         choices: ['Engineer', 'Intern']
     }
 ]
+const engineerQs = [
+    {
+        type: 'input',
+        name: 'engName',
+        messgae: 'What is the engineer/s name?'
+    },
+    {
+        type: 'input',
+        name: 'engID',
+        messgae: 'What is the engineer/s employee ID?'
+    },
+    {
+        type: 'input',
+        name: 'engEmail',
+        messgae: 'What is the engineer/s email?'
+    },
+    {
+        type: 'input',
+        name: 'engGitHub',
+        messgae: 'What is the engineer/s github username?'
+    }
+]
+const internQs = [
+    {
+        type: 'input',
+        name: 'internName',
+        messgae: 'What is the intern/s name?'
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        messgae: 'What is the intern/s employee ID?'
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        messgae: 'What is the intern/s email?'
+    },
+    {
+        type: 'input',
+        name: 'internGitHub',
+        messgae: 'What is the intern/s github username?'
+    }
+]
+
+function cont()
+{
+    inquirer 
+        .prompt(newMember)
+        .then(// write the other questions to insert in here with an if, then statement)
+}
+
+function 
+
+
+
+//Add where and what the file should be called
+function writeToFile(input) {
+    fs.writeFileSync(`${input.fileName}.md`, markdown(input))
+}
 
 // create a function for the start of the code
+function init() {
+    inquirer
+    .prompt(start)
+    .then(menu)
+    .then((input, list) => {
+        if (`${list.choices}` == 'Finished'){
+          writeToFile(input);  
+        } else {
+            cont();
+        }
+    });
+};
+
+// Function call to initialize app
+init();
